63d861f5354522b314a44ab5613e50dd684953c6
kymdennis3 <kymdennis3@gmail.com>
2022-11-04 18:32:18 +0300

    introduced parameter age

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
         //method = a block of code that is executed whenever it is called upon
 
         String name = "Kimtai";
+        int age = 25;
 
         //values or variables parsed to a method are referred to as arguments and can be confused with parameters
         //You need a matching set of parameters
-        hello(name);
+        hello(name,age);
      }
-    static void hello(String name) {
+    static void hello(String name,Integer age) {
 
         System.out.println("Hello "+name); //with parameters you dont need to keep the name of the values consistent
+        System.out.println("You are "+age +" years old");
 }}
