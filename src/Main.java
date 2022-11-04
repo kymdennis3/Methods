1c065de57fbcf36eba16378d7596c1652132b20b
kymdennis3 <kymdennis3@gmail.com>
2022-11-04 18:26:01 +0300

    introduced parameters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        //method = a block of code that is executed whenever it is called upon
+
+        String name = "Kimtai";
+
+        //values or variables parsed to a method are referred to as arguments and can be confused with parameters
+        //You need a matching set of parameters
+        hello(name);
+     }
+    static void hello(String name) {
+
+        System.out.println("Hello "+name); //with parameters you dont need to keep the name of the values consistent
+}}
